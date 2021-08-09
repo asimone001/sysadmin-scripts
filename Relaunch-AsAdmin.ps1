30ba36eee72ff01777c0237b7f5b1b424a08a516
asimone <53988692+asimone001@users.noreply.github.com>
2021-08-09 16:49:43 -0400

    Create Relaunch-AsAdmin.ps1

---
+++ Relaunch-AsAdmin.ps1
@@ -0,0 +1,9 @@
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{   
+    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    Start-Process powershell -Verb runAs -ArgumentList $arguments
+    Break
+}
+Set-Location -Path $PSScriptRoot
+Write-Host $PSScriptRoot
+Read-Host "Thank you for launching the script. Please press Enter to continue."
