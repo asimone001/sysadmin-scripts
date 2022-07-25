f9981014c4dc9d8e3e15652cbd13bbd41bdec269
asimone <53988692+asimone001@users.noreply.github.com>
2022-07-25 12:13:01 -0400

    fix: spelling

--- Uninstall-Silverlight.ps1
+++ Uninstall-Silverlight.ps1
@@ -2,7 +2,7 @@
 .SYNOPSIS
     A simple script to remove Silverlight from any computer it is installed on.
 .DESCRIPTION
-    This script searchies for any package with Silverlight in the name, and uninstalls it.
+    This script searches for any package with Silverlight in the name, and uninstalls it.
 .NOTES
     This function requires the NuGet Package Provider. The use of -Force is intended to bypass the normal install 
     prompt so it can be used in an automated fashion.
