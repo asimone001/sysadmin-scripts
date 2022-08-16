c71261ab7a4993972e281964a7f11df89473277d
asimone <53988692+asimone001@users.noreply.github.com>
2022-08-16 16:21:41 -0400

    break one-liner into multiple lines

--- Install-GoogleDriveFS.ps1
+++ Install-GoogleDriveFS.ps1
@@ -1,1 +1,6 @@
-$Path = $env:TEMP; $Installer = "gdfs.exe"; Invoke-WebRequest "https://dl.google.com/drive-file-stream/GoogleDriveFSSetup.exe" -OutFile $Path\$Installer; Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait; Remove-Item $Path\$Installer
+$Path = $env:TEMP
+$Installer = "gdfs.exe"
+
+Invoke-WebRequest "https://dl.google.com/drive-file-stream/GoogleDriveFSSetup.exe" -OutFile $Path\$Installer
+Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait
+Remove-Item $Path\$Installer
