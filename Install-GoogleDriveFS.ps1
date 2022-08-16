eda95a4b1e6d49ae46c7053836d24883082fc5f2
asimone <53988692+asimone001@users.noreply.github.com>
2022-08-16 16:22:27 -0400

    added comments where needed

--- Install-GoogleDriveFS.ps1
+++ Install-GoogleDriveFS.ps1
@@ -1,6 +1,10 @@
+# Variables
 $Path = $env:TEMP
 $Installer = "gdfs.exe"
 
+# Download and install the program
 Invoke-WebRequest "https://dl.google.com/drive-file-stream/GoogleDriveFSSetup.exe" -OutFile $Path\$Installer
 Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait
+
+# Clean up installer after it is finished.
 Remove-Item $Path\$Installer
